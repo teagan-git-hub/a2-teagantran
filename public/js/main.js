dab874f68ffc92f553ddfe065902d2a0bd48a5bc
teagan-git-hub <teagan.m.tran@gmail.com>
2026-09-03 14:25:33 -0400

    Render saved credentials as list

--- public/js/main.js
+++ public/js/main.js
@@ -25,4 +25,19 @@ const submit = async function( event ) {
   })
 
   const arr = await response.json()
+  ul.innerHTML = ''
+  for ( let item of arr ) {
+    const li = document.createElement('li')
+    const name = item.username ?? item.yourname ?? ''
+    const pass = item.password ?? ''
+    li.innerText = name + (pass ? ' — ' + pass : '')
+    ul.appendChild( li )
+  }
+}
+
+window.onload = function() {
+  const button = document.querySelector('button')
+  button.onclick = submit
+  ul = document.createElement( 'ul' )
+  document.body.appendChild( ul )
 }
